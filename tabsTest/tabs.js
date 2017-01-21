a81c801fabc8ca2791025ebcc9fe35377edf0987
christopherhilts@gmail.com <christopherhilts@gmail.com>
2017-01-21 18:25:20 -0500

    Added json parsing and loading of iframes and injecting js into DOM structure of webviews

--- tabsTest/tabs.js
+++ tabsTest/tabs.js
@@ -1,11 +1,54 @@
 const TabGroup = require("electron-tabs")
+var fs = require('fs')
 
+var chatData;
+var currentChat;
 
-function init(){
+function addTab(){
     tabGroup = new TabGroup()
     let testTab = tabGroup.addTab({
         title: "test",
-        src: "http://google.ca",
-        visible: true
+        visible: true,
+        webviewAttributes: {
+            partition: "persist:google"
+        }
     })
+}
+
+function addIframe(url){
+    var newDiv = document.getElementById("frames")
+    var li = document.createElement('li')
+    li.setAttribute("style", "position: absolute; list-style-type: none;")
+    var newWebview = document.createElement('webview')
+    newWebview.setAttribute("src", url)
+    newWebview.setAttribute("style", "display:inline-flex; width:640px; height:480px")
+    li.appendChild(newWebview)
+    newDiv.appendChild(li)
+    return newWebview
+}
+
+window.onload = function(){
+    fs.readFile('test.json', (err,data) =>{
+        chatData = JSON.parse(data)
+        for(var service in chatData){
+            var webView = addIframe(chatData[service].url)
+            currentChat = service
+            webView.addEventListener('dom-ready', (e) =>{
+                var js = 'document.getElementById("pass").addEventListener("click", (e)=>{console.log("click")})' 
+                webView.executeJavaScript(js)
+                webView.openDevTools()
+                
+            })
+            
+        }
+    });
+}
+
+
+
+window.onfocus = function(e){
+    e
+}
+
+window.onkeydown = function(e){
 }
