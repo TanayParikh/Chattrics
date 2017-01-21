c98795a82a445387222fd8d692e8300d22585fed
christopherhilts@gmail.com <christopherhilts@gmail.com>
2017-01-21 13:18:05 -0500

    Added other chat tabs

--- tabs.js
+++ tabs.js
@@ -2,9 +2,28 @@ const TabGroup = require("electron-tabs")
 
 function initPage (){
     let tabGroup = new TabGroup();
-    let tab = tabGroup.addTab({
-        title: "Test",
-        src: "http://github.com",
-        visible: true
+    let messenger = tabGroup.addTab({
+        title: "Messanger",
+        src: "https://www.messenger.com/login.php",
+        visible: true,
+        webviewAttributes: {
+            partition: "persist:facebook"
+        }
     });
+    let messangerView = messenger.webview;
+    messangerView.addEventListener('did-finish-load', onDomLoad());
+    let whatsApp = tabGroup.addTab({
+        title: "Whats App",
+        src: "https://web.whatsapp.com/",
+        visible: true,
+    });
+    let hangouts = tabGroup.addTab({
+        title: "Hangouts",
+        src: "https://accounts.google.com/ServiceLogin?service=talk&passive=1209600&continue=https://hangouts.google.com/&followup=https://hangouts.google.com/#identifierl",
+        visible: true
+    })
+}
+
+function onDomLoad(){
+    console.log("Loaded")
 }
