07ec73305f59d82e8d6f6a6b2265b9f368df302f
Tanay <tanay@webexposure.ca>
2017-01-21 18:32:12 -0500

    Added live-reload functionality for dev env

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
+require("electron-reload")(__dirname);
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
@@ -53,5 +55,3 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
