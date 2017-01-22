cc3be0f13f073ec0a4060fd12300ef8f53291dfd
Tanay <tanay@webexposure.ca>
2017-01-22 01:57:10 -0500

    Hid menu/dock bar

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600, icon:'Artwork/Logo/app_icon.png'})
+  win.setMenu(null);
+
 
   // and load the index.html of the app.
   win.loadURL(url.format({
