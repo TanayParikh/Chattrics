f8d03eee88df1f078fad2b863aaff06c37cd717f
Tanay <tanay@webexposure.ca>
2017-01-22 02:51:34 -0500

    Added Window Max, Min, Close buttons

--- app.js
+++ app.js
@@ -113,3 +113,35 @@ window.onload = function(){
 var vm = new Vue({
   el: '#root'
 });
+
+(function () {
+
+      const remote = require('electron').remote;
+
+      function init() {
+        document.getElementById("min-btn").addEventListener("click", function (e) {
+          const window = remote.getCurrentWindow();
+          window.minimize();
+        });
+
+        document.getElementById("max-btn").addEventListener("click", function (e) {
+          const window = remote.getCurrentWindow();
+          if (!window.isMaximized()) {
+            window.maximize();
+          } else {
+            window.unmaximize();
+          }
+        });
+
+        document.getElementById("close-btn").addEventListener("click", function (e) {
+          const window = remote.getCurrentWindow();
+          window.close();
+        });
+      };
+
+      document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+          init();
+        }
+      };
+})();
