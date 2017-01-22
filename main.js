045035945081ae8e2e2f925055f139adc7540e17
Tanay <tanay@webexposure.ca>
2017-01-22 02:26:36 -0500

    Added custom scroll bar and resized default window

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600, icon:'Artwork/Logo/app_icon.png'})
-  win.setMenu(null);
+  win = new BrowserWindow({width: 1190, height: 720, icon:'Artwork/Logo/app_icon.png', frame:false})
+  //win.setMenu(null);
 
 
   // and load the index.html of the app.
